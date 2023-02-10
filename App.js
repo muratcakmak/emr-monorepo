2c8b3de687d9d63e0e07fcde7f566100e776ebf7
Oguzhan Cakmak <oguzhanmc@gmail.com>
2023-02-10 15:24:00 +0300

    Initial commit

--- App.js
+++ App.js
@@ -1,17 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Image } from 'expo-image';
-import { LinearGradient } from 'expo-linear-gradient';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Image } from "expo-image";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <LinearGradient
-        colors={['#232526', '#66686a']}
-        style={{ position: 'absolute', left: 0, right: 0, top: 0, bottom: 0 }}
+        colors={["#232526", "#66686a"]}
+        style={{ position: "absolute", left: 0, right: 0, top: 0, bottom: 0 }}
       />
 
-      <View style={{ position: 'absolute', bottom: 50, left: 0, right: 0, alignItems: 'center' }}>
+      <View
+        style={{
+          position: "absolute",
+          bottom: 50,
+          left: 0,
+          right: 0,
+          alignItems: "center"
+        }}
+      >
         <Image
           style={{ width: 100, height: 100 }}
           contentFit="contain"
@@ -25,11 +33,21 @@ export default function App() {
         source="https://d33wubrfki0l68.cloudfront.net/554c3b0e09cf167f0281fda839a5433f2040b349/ecfc9/img/header_logo.svg"
       />
 
-      <Text style={{ fontSize: 22, marginTop: 15, color: '#fff' }}>
-        New Architecture: <Text style={{ fontWeight: 'bold' }}>Enabled</Text>
+      <Text
+        style={{
+          fontSize: 20,
+          color: "#fff",
+          fontWeight: "bold",
+          fontFamily: "San Francisco"
+        }}
+      >
+        Oguzhan Murat Cakmak
+      </Text>
+      <Text style={{ fontSize: 22, marginTop: 15, color: "#fff" }}>
+        New Architecture: <Text style={{ fontWeight: "bold" }}>Enabled</Text>
       </Text>
 
-      < StatusBar style="auto" />
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -37,8 +55,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
